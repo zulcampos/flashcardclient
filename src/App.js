d56ed77acb8536fa2c5aefd7898619e15c8b1ede
Zulema Campos <zulemacampos@Zulemas-Air.fios-router.home>
2022-05-17 15:44:19 -0400

    Day 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import QuestionForm from './components/quiz/QuestionForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QuestionForm></QuestionForm>
   );
 }
 
